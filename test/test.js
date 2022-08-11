102ace003fc0eafb1c927a6778c30f2d752a396e
Dan-Sillva <danilo.official22@hotmail.com>
2022-08-11 10:16:46 -0300

    searching in google aboult nodejs

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 // exportando a build do selenium e o modulo do firefox
-const {Builder} = require('selenium-webdriver')
+const {Builder, By} = require('selenium-webdriver')
 const firefox = require('selenium-webdriver/firefox')
+const { DriverService } = require('selenium-webdriver/remote')
 
 // criando o serviço do firefox(passando o path do webdrive) e criando o drive em cima
 const service = new firefox.ServiceBuilder('/home/dan/Estudos/seleniumjs/webDrivers/geckodriver')
@@ -11,6 +12,13 @@ async function openFirefox(){
     try {
         
         await driver.get('https://www.google.com')
+
+        let searchBar = await driver.findElement(By.name('q'))
+        let btnGo = await driver.findElement(By.xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]'))
+        
+        await searchBar.sendKeys('nodejs')
+        await btnGo.click()
+
         await driver.quit()
 
     } catch (error) {
